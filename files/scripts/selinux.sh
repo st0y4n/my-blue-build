25da1d17fa238c841b5c916cd0bb02d2c2ad65d1
st0y4n <stoyang2005@gmail.com>
2026-05-23 20:31:09 +0300

    fix

--- files/scripts/selinux.sh
+++ files/scripts/selinux.sh
@@ -3,8 +3,8 @@ set -oue pipefail
 
 echo "Baking custom SELinux policies..."
 
-semodule -X 200 -i ./selinux/container200.pp
-semodule -X 200 -i ./selinux/flatpak200.pp
-semodule -X 200 -i ./selinux/waydroid200.pp
+semodule -X 200 -i ./selinux/container.cil
+semodule -X 200 -i ./selinux/flatpak.cil
+semodule -X 200 -i ./selinux/waydroid.cil
 
 echo "SELinux policies baked successfully."
