4eb2d4a2eb1d92978770a1e8fa74e0a7daf47eda
st0y4n <stoyang2005@gmail.com>
2026-05-29 13:42:14 +0300

    Silverblue Cachy

--- files/scripts/custom-kernel.sh
+++ files/scripts/custom-kernel.sh
@@ -6,7 +6,7 @@ echo "=== STARTING CACHYOS + SECURE BOOT SETUP ==="
 ##################################
 # 1. CACHY KERNEL SETUP
 ##################################
-KERNEL_TYPE="${1:-cachyos-lto}"
+KERNEL_TYPE="${1:-cachyos}"
 
 case "${KERNEL_TYPE}" in
 cachyos-lto)
